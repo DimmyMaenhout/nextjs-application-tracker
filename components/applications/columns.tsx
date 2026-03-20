3761bee970e8be5507499508f8a0c62cf967a3b9
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-20 20:08:10 +0100

    Added a DataTable and Columns using Shadcn

---
+++ components/applications/columns.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Application } from "@/models/Application";
+import { ApplicationStatus, StatusColors } from "@/models/ApplicationStatus";
+import { formatDate } from "@/util/date";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const Columns: ColumnDef<Application>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status: ApplicationStatus = row.getValue("status");
+      const color = StatusColors[status];
+
+      return <div className={`${color} rounded-[8px]  font-bold text-center`}>{status}</div>;
+    },
+  },
+  {
+    accessorKey: "jobTitle",
+    header: "Job Title",
+  },
+  {
+    accessorKey: "companyName",
+    header: "Company Name",
+  },
+  {
+    accessorKey: "jobUrl",
+    header: "Job Url",
+  },
+  {
+    accessorKey: "appliedAt",
+    header: "Applied at",
+    cell: ({ row }) => {
+      const appliedAtDate = new Date(row.getValue("appliedAt"));
+      const formatted = formatDate(appliedAtDate);
+
+      return <div>{formatted}</div>;
+    },
+  },
+];
