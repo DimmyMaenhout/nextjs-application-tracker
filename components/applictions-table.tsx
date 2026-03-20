a824cf637245960699c2336ccabb8abc8f2e9ba1
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-20 10:32:22 +0100

    feat: initial commit

---
+++ components/applictions-table.tsx
@@ -0,0 +1,11 @@
+import { Application } from "@/models/Application";
+
+type ApplicationsTableProps = {
+  applications: Application[];
+};
+
+export default function ApplictionsTable({
+  applications,
+}: ApplicationsTableProps) {
+  return <div></div>;
+}
