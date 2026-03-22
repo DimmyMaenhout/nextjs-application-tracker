bef65c430ac13c2b8ad7649030c3e26c349c9b79
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-22 20:56:33 +0100

    Added and styled the submit button

--- app/applications/new/page.tsx
+++ app/applications/new/page.tsx
@@ -21,43 +21,54 @@ export default function NewApplicationPage() {
   return (
     <form>
       <div className="flex flex-col gap-4">
-        <div className="flex flex-row gap-4">
-          <FormField name="jobTitle" label="Job Title" className="flex-1">
-            <input className={inputStyle} type="text" />
-          </FormField>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row gap-4">
+            <FormField name="jobTitle" label="Job Title" className="flex-1">
+              <input className={inputStyle} type="text" />
+            </FormField>
 
-          <FormField name="companyName" label="Company Name" className="flex-1">
-            <input className={inputStyle} type="text" />
+            <FormField
+              name="companyName"
+              label="Company Name"
+              className="flex-1"
+            >
+              <input className={inputStyle} type="text" />
+            </FormField>
+          </div>
+
+          <div className="flex flex-row gap-4">
+            <FormField
+              name="contactName"
+              label="Contact Name"
+              className="flex-1"
+            >
+              <input className={inputStyle} type="text" />
+            </FormField>
+
+            <FormField
+              name="contactEmail"
+              label="Contact Email"
+              className="flex-1"
+            >
+              <input className={inputStyle} type="email" />
+            </FormField>
+          </div>
+
+          <div className="flex flex-row gap-4">
+            <FormField name="location" label="Location" className="flex-1">
+              <input className={inputStyle} type="text" />
+            </FormField>
+
+            <FormField name="source" label="Source" className="flex-1">
+              <input className={inputStyle} type="text" />
+            </FormField>
+          </div>
+
+          <FormField name="notes" label="Notes">
+            <textarea className={inputStyle} name="notes" rows={5} id="notes" />
           </FormField>
         </div>
-
-        <div className="flex flex-row gap-4">
-          <FormField name="contactName" label="Contact Name" className="flex-1">
-            <input className={inputStyle} type="text" />
-          </FormField>
-
-          <FormField
-            name="contactEmail"
-            label="Contact Email"
-            className="flex-1"
-          >
-            <input className={inputStyle} type="email" />
-          </FormField>
-        </div>
-
-        <div className="flex flex-row gap-4">
-          <FormField name="location" label="Location" className="flex-1">
-            <input className={inputStyle} type="text" />
-          </FormField>
-
-          <FormField name="source" label="Source" className="flex-1">
-            <input className={inputStyle} type="text" />
-          </FormField>
-        </div>
-
-        <FormField name="notes" label="Notes">
-          <textarea className={inputStyle} name="notes" rows={5} id="notes" />
-        </FormField>
+        <button className="self-end bg-blue-500 hover:bg-blue-700 text-white px-4 py-2  border-black rounded-2xl" type="submit">Submit</button>
       </div>
     </form>
   );
