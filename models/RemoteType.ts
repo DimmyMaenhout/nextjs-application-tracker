148083a2397472d9e9e728c06c83f808582ad0a6
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-04-27 16:56:26 +0200

    Updated model RemoteType with array so we can use it in the SelectField

--- models/RemoteType.ts
+++ models/RemoteType.ts
@@ -1,1 +1,5 @@
 export type RemoteType = "on_site" | "hybrid" | "remote";
+
+export const RemoteTypes = ["on_site", "hybrid", "remote"] as const;
+
+export type RemoteTypeArrayValues = (typeof RemoteTypes)[number];
