05d5c37320d82ed0cf4daae7f4fc49424f43ad27
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-04-29 11:25:25 +0200

    Created a lib/constants/Applications file for the application StatusColors

---
+++ lib/constants/Applications.ts
@@ -0,0 +1,13 @@
+import { ApplicationStatus } from "../generated/prisma/enums";
+
+export const StatusColors: Record<ApplicationStatus, string> = {
+  wishlist: "bg-gray-200 text-gray-600",
+  to_apply: "bg-blue-200 text-blue-600",
+  applied: "bg-yellow-200 text-yellow-600",
+  interview: "bg-purple-200 text-purple-600",
+  offer: "bg-green-200 text-green-600",
+  rejected: "bg-red-200 text-red-600",
+  accepted: "bg-emerald-200 text-emerald-600",
+  withdrawn: "bg-slate-200 text-slate-600",
+  ghosted: "bg-zinc-200 text-zinc-600",
+};
