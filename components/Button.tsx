ace98e93bc34e15ee1e69f24a6f3e2bd5e41c396
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-04-29 13:13:34 +0200

    Updated Button component to have variants (primary & danger)

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,29 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 
+type ButtonVariant = "primary" | "danger";
+
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  className?: string;
+  variant?: ButtonVariant;
+};
+
+const variants: Record<ButtonVariant, string> = {
+  primary: "bg-(--color-main-button) hover:bg-(--color-main-button-hover)",
+  danger: "bg-(--color-danger-button) hover:bg-(--color-danger-button-hover)",
 };
 
 export default function Button({
   children,
   className,
+  variant = "primary",
   ...props
 }: PropsWithChildren<ButtonProps>) {
   return (
     <button
-      className={`px-4 py-2 bg-(--color-main-button) hover:bg-(--color-main-button-hover) text-white font-bold rounded-2xl ${className}`}
+      className={`
+        px-4 py-2  text-white font-bold rounded-2xl
+        ${variants[variant]}
+        ${className}
+      `}
       {...props}
     >
       {children}
