bf1a5e719eddb729723b07f57d976801da993ced
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-04-30 09:44:57 +0200

    Updated component ApplicationDetailRow to have a fallbackValue property

--- components/ApplicationDetailRow.tsx
+++ components/ApplicationDetailRow.tsx
@@ -1,16 +1,18 @@
 type ApplicationDetailRowProps = {
   title: string;
-  value: string;
+  value: string | null;
+  fallbackValue?: string;
 };
 
 export default function ApplicationDetailRow({
   title,
   value,
+  fallbackValue = "N/A",
 }: ApplicationDetailRowProps) {
   return (
     <div className="flex">
       <p className="font-bold w-40">{title}</p>
-      <p>{value}</p>
+      <p>{value ? value : fallbackValue}</p>
     </div>
   );
 }
