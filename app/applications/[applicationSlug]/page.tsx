c1887c648416e095eefd5100d547e0ee25b8a668
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-04-30 12:19:00 +0200

    Refactor: Migrate ApplicationDetailsPage to card layout

--- app/applications/[applicationSlug]/page.tsx
+++ app/applications/[applicationSlug]/page.tsx
@@ -2,15 +2,23 @@ import { deleteApplication } from "@/actions/Application";
 import ApplicationDetailRow from "@/components/ApplicationDetailRow";
 import Button from "@/components/Button";
 import NoteCard from "@/components/noteCard";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { APPLICATION_STATUS_META } from "@/lib/constants/Applications";
 import { prisma } from "@/lib/prisma";
+import { Plus, Trash2 } from "lucide-react";
 
 export default async function ApplicationDetailsPage({
   params,
 }: {
   params: Promise<{ applicationSlug: string }>;
 }) {
-  const h2Styling = "font-bold text-2xl text-[#1e8fa3] mb-8";
+  const h2Styling = "font-extrabold text-2xl text-[#1e8fa3]";
   const { applicationSlug } = await params;
 
   const applicationDetail = await prisma.application.findFirst({
@@ -23,74 +31,85 @@ export default async function ApplicationDetailsPage({
   }
 
   return (
-    <div className="p-8 flex flex-col">
-      <div className="w-full rounded-2xl shadow-md p-4 border border-gray-200 hover:shadow-lg transition">
-        <div className="flex justify-between">
-          <h2 className={h2Styling}>Details</h2>
-          <form action={deleteApplication.bind(null, applicationSlug)}>
-            <Button variant="danger">Delete</Button>
-          </form>
-        </div>
-        <div className="flex justify-around ">
-          <div>
-            <ApplicationDetailRow
-              title="Company Name"
-              value={applicationDetail.companyName}
-            />
+    <div className="flex flex-col gap-8  ">
+      <Card>
+        <CardHeader>
+          <CardTitle className={h2Styling}>Details</CardTitle>
+          <CardAction>
+            <form action={deleteApplication.bind(null, applicationSlug)}>
+              <Button variant="danger">
+                <Trash2 />
+              </Button>
+            </form>
+          </CardAction>
+        </CardHeader>
 
-            <ApplicationDetailRow
-              title="Job Title"
-              value={applicationDetail.jobTitle}
-            />
+        <CardContent>
+          <div className="mx-auto w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8">
+            {/* Linker kolom */}
+            <div>
+              <ApplicationDetailRow
+                title="Company Name"
+                value={applicationDetail.companyName}
+              />
 
-            <ApplicationDetailRow
-              title="Location"
-              value={applicationDetail.location}
-            />
+              <ApplicationDetailRow
+                title="Job Title"
+                value={applicationDetail.jobTitle}
+              />
 
-            <ApplicationDetailRow
-              title="Source"
-              value={applicationDetail.source}
-            />
+              <ApplicationDetailRow
+                title="Location"
+                value={applicationDetail.location}
+              />
+
+              <ApplicationDetailRow
+                title="Source"
+                value={applicationDetail.source}
+              />
+            </div>
+
+            {/* Rechter kolom */}
+            <div>
+              <ApplicationDetailRow
+                title="Applied"
+                value={
+                  applicationDetail.appliedAt
+                    ? new Date(applicationDetail.appliedAt).toLocaleDateString()
+                    : ""
+                }
+              />
+
+              <ApplicationDetailRow
+                title="Status"
+                value={APPLICATION_STATUS_META[applicationDetail.status].label}
+              />
+
+              <ApplicationDetailRow
+                title="Contact Name"
+                value={applicationDetail.contactName}
+              />
+
+              <ApplicationDetailRow
+                title="Contact Email"
+                value={applicationDetail.contactEmail}
+              />
+            </div>
           </div>
-
-          {/* 2de kolom /  */}
-
-          <div>
-            <ApplicationDetailRow
-              title="Applied"
-              value={
-                applicationDetail.appliedAt
-                  ? new Date(
-                      applicationDetail.appliedAt ?? "",
-                    ).toLocaleDateString()
-                  : ""
-              }
-            />
-
-            <ApplicationDetailRow
-              title="Status"
-              value={APPLICATION_STATUS_META[applicationDetail.status].label}
-            />
-
-            <ApplicationDetailRow
-              title="Contact Name"
-              value={applicationDetail.contactName}
-            />
-
-            <ApplicationDetailRow
-              title="Contact Email"
-              value={applicationDetail.contactEmail}
-            />
-          </div>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
 
       {/* Notes */}
-      <div className="flex flex-col gap-4 mt-8">
-        <div className="w-full bg-white rounded-2xl p-4">
-          <h2 className={h2Styling}>Notes</h2>
-
+      <Card>
+        <CardHeader>
+          <CardTitle className={h2Styling}>Notes</CardTitle>
+          <CardAction>
+            <Button>
+              <Plus />
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
           {applicationDetail.notes.length > 0 ? (
             <div className="flex flex-col gap-4 items-center">
               {applicationDetail.notes.map((note) => (
@@ -102,8 +121,8 @@ export default async function ApplicationDetailsPage({
               You don't have any notes for this application.
             </p>
           )}
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
