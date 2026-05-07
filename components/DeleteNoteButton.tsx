43ea2c5d0dc61622aa866831df571305d80d05b1
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-05-07 15:18:52 +0200

    Added the DeleteNoteButton component

---
+++ components/DeleteNoteButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { deleteApplication, deleteNote } from "@/actions/Application";
+import { useConfirm } from "./modals/confirm-provider";
+import Button from "./Button";
+import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Note } from "@/lib/generated/prisma/client";
+
+type DeleteNoteButtonProps = Pick<Note, "id" | "applicationId">;
+
+export default function DeleteNoteButton({
+  id,
+  applicationId,
+}: DeleteNoteButtonProps) {
+  const { confirm } = useConfirm();
+  const router = useRouter();
+
+  const handleDelete = () => {
+    confirm({
+      title: "Verwijder notitie?",
+      description: "Deze actie kan niet ongedaan gemaakt worden.",
+      onConfirm: async () => {
+        await deleteNote(id, applicationId);
+      },
+    });
+  };
+
+  return (
+    <Button variant="danger" onClick={handleDelete}>
+      <Trash2 />
+    </Button>
+  );
+}
